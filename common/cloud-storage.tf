9408495b1ca34c3ceecfbcb13ce3dd64ac7e2acb
Antonio Roman <e_aroman@mercadona.com>
2024-01-31 17:08:09 +0100

    storage bucket class changed to NEARLINE

--- common/cloud-storage.tf
+++ common/cloud-storage.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "test_crossplane_bucket" {
     name          = "test-crossplane-bucket-qualtio"
     location      = "US"
-    storage_class = "STANDARD"
+    storage_class = "NEARLINE"
 
     uniform_bucket_level_access = true
 }
