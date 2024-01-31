587828e37ae9caaa2ada45c086a201dc628a26a1
Antonio Roman <e_aroman@mercadona.com>
2024-01-31 16:47:33 +0100

    bucket name changed

--- common/cloud-storage.tf
+++ common/cloud-storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "test_crossplane_bucket" {
-    name          = "test.crossplane.bucket.qualtio"
+    name          = "test-crossplane-bucket-qualtio"
     location      = "US"
     storage_class = "STANDARD"
 
