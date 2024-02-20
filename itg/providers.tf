76d73075067921dc6633d640eafe66941e5fef14
Antonio Roman <e_aroman@mercadona.com>
2024-02-20 16:46:02 +0100

    provider file added to itg forlder

---
+++ itg/providers.tf
@@ -0,0 +1,15 @@
+provider "google" {
+    credentials = "qualtio-pre-credentials.json"
+    project = "qualtio-dev"
+    region  = "us-central1"
+    zone    = "us-central1-c"
+}
+
+// Modules _must_ use remote state. The provider does not persist state.
+terraform {
+    backend "kubernetes" {
+    secret_suffix     = "providerconfig-default"
+    namespace         = "crossplane-system"
+    in_cluster_config = true
+    }
+}
