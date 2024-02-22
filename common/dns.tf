2821b84cd1a45e6dca7bdd1251f15ad277051f79
Antonio Roman <e_aroman@mercadona.com>
2024-02-22 10:27:56 +0100

    unused files and clud storage deleted

--- common/dns.tf
+++
@@ -1,8 +0,0 @@
-# resource "google_dns_record_set" "dns_record" {
-#   name = "terraform-dns."
-#   type = "A"
-#   ttl  = 300
-
-#   managed_zone = "us-central1-c"
-#   rrdatas = [""]
-# }
--- dev/cloud-storage.tf
+++ dev/cloud-storage.tf
@@ -1,7 +1,7 @@
-resource "google_storage_bucket" "test_crossplane_bucket2" {
-    name          = "test-crossplane-bucket-qualtio-dev2"
-    location      = var.location
-    storage_class = "STANDARD"
+# resource "google_storage_bucket" "test_crossplane_bucket2" {
+#     name          = "test-crossplane-bucket-qualtio-dev2"
+#     location      = var.location
+#     storage_class = "STANDARD"
 
-    uniform_bucket_level_access = true
-}
+#     uniform_bucket_level_access = true
+# }
